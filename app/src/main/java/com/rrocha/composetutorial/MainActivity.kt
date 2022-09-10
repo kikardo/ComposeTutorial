69a3cd414eba636e7a58eeb978e0054a6caab242
Ricardo da Rocha <ricardo.daroc@gmail.com>
2022-09-10 20:11:18 -0300

    Class 1: Hello world

--- app/src/main/java/com/rrocha/composetutorial/MainActivity.kt
+++ app/src/main/java/com/rrocha/composetutorial/MainActivity.kt
@@ -16,28 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Text("Hello world")
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeTutorialTheme {
-        Greeting("Android")
     }
 }
