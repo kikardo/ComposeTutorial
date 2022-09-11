399488f4682ff4929b8532529a4c832e5f3293fe
Ricardo da Rocha <ricardo.daroc@gmail.com>
2022-09-10 23:58:11 -0300

    Class 3: Material Design and Class 4:Lists and animations

--- app/src/main/java/com/rrocha/composetutorial/MainActivity.kt
+++ app/src/main/java/com/rrocha/composetutorial/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.rrocha.composetutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -21,7 +28,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Android", "Jetpack Compose"))
+            ComposeTutorialTheme() {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Conversation(SampleData.conversationSample)
+                }
+            }
         }
     }
 
@@ -39,21 +50,75 @@ class MainActivity : ComponentActivity() {
                     .size(40.dp)
                     // Clip image to be shaped as a circle
                     .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
             )
             // Add a horizontal space between the image and the column
             Spacer(modifier = Modifier.width(8.dp))
 
-            Column() {
-                Text(msg.author)
+            // We keep track if the message is expanded or not in this
+            // variable
+            var isExpanded by remember { mutableStateOf(false) }
+            // surfaceColor will be updated gradually from one color to the other
+            val surfaceColor by animateColorAsState(
+                if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+            )
+            // We toggle the isExpanded variable when we click on this Column
+            Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+                Text(
+                    msg.author, color = MaterialTheme.colors.secondaryVariant,
+                    style = MaterialTheme.typography.subtitle2
+                )
                 Spacer(modifier = Modifier.height(4.dp))
-                Text(msg.body)
+                Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    elevation = 1.dp,
+                    // surfaceColor color will be changing gradually from primary to surface
+                    color = surfaceColor,
+                    // animateContentSize will change the Surface size gradually
+                    modifier = Modifier.animateContentSize().padding(1.dp)
+                ) {
+                    Text(
+                        msg.body,
+                        modifier = Modifier.padding(all = 4.dp),
+                        // If the message is expanded, we display all its content
+                        // otherwise we only display the first line
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                        style = MaterialTheme.typography.body2
+                    )
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun Conversation(messages: List<Message>) {
+        LazyColumn {
+            items(messages) { message ->
+                MessageCard(message)
+            }
+        }
+    }
+
+    @Preview(name = "Light Mode")
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true,
+        name = "Dark Mode"
+    )
+    @Composable
+    fun PreviewMessageCard() {
+        ComposeTutorialTheme() {
+            Surface(modifier = Modifier.fillMaxSize()) {
+                MessageCard(Message("Coleague", "Hey, take a look at Jetpack Compose, it's great!"))
             }
         }
     }
 
     @Preview
     @Composable
-    fun PreviewMessageCard() {
-        MessageCard(Message("Coleague", "Hey, take a look at Jetpack Compose, it's great!"))
+    fun PreviewConversation() {
+        ComposeTutorialTheme {
+            Conversation(SampleData.conversationSample)
+        }
     }
 }
